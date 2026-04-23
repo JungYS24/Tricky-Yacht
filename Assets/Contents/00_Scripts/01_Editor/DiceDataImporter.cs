8ce59eda0b49270d41fd1dd4ea2d96d58f6f56f1
JungYoonSung <ys47497568@gmail.com>
2026-04-23 22:06:27 +0900

    feat. 패키징 세팅 : 빌드

--- Assets/Contents/00_Scripts/01_Editor/DiceDataImporter.cs
+++ Assets/Contents/00_Scripts/01_Editor/DiceDataImporter.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿#if UNITY_EDITOR
+using UnityEngine;
 using UnityEditor;
 using System.IO;
 using System.Collections.Generic;
@@ -56,3 +57,4 @@ public class DiceDataImporter : EditorWindow
         }
     }
 }
+#endif
