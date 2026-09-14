be132b75ba2e4a35e6e5b0b0d5bb0fcd4fc040ff
KimBH <kimbh0437@gmail.com>
2026-09-14 18:50:51 +0900

    feat: 캐릭터 초기 체력 추가

--- Assets/Contents/00_Scripts/03_Data/Character/CharacterData.cs
+++ Assets/Contents/00_Scripts/03_Data/Character/CharacterData.cs
@@ -4,11 +4,15 @@ public class CharacterData : ScriptableObject
 {
     [SerializeField] private string characterName;
 
+    [SerializeField] private int initialHP;
+
     [SerializeField] private DiceItemSO dedicateDice;
 
     [SerializeField] private UnlockConditionManager.UnlockFlag unlockCondition;
 
     public string CharacterName => characterName;
+
+    public int InitialHP => initialHP;
 
     public DiceItemSO DedicateDiceData => dedicateDice;
 
